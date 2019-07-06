cc519403dfd4e355ae86117d04eda0db9bc36b44
Viktor Simon-Papp <simonviktor.74@gmail.com>
2019-07-06 09:14:03 +0100

    Added binder controller

--- arduino/blinder_controller/blinder_controller/blinder_controller.ino
+++ arduino/blinder_controller/blinder_controller/blinder_controller.ino
@@ -1,39 +1,88 @@
+#include <EEPROM.h>
+#include "EEPROMAnything.h"
 // Define pin connections & motor's steps per revolution
-const int dirPin = 2;
-const int stepPin = 3;
+#define DIRPIN 2
+#define STEPPIN 3
+#define UPBUTTONPIN 5
+#define DOWNBUTTONPIN 6
+#define UPLEDPIN 7
+#define DOWNLEDPIN 8
+
 const int stepsPerRevolution = 200;
+bool directionUp = true;
+int motorSpeed = 1000;
+int minSteps = 0;
+int maxSteps = 20;
+int actualSteps = 0;
+struct config_t
+{
+    int motorSpeed;
+    int actualSteps;
+} configuration;
+
+int stepsAddress = 0;
 
 void setup()
 {
+  Serial.begin(9600);
   // Declare pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(STEPPIN, OUTPUT);
+  pinMode(DIRPIN, OUTPUT);
+  pinMode(UPLEDPIN, OUTPUT);
+  pinMode(DOWNLEDPIN, OUTPUT);
+
+  pinMode(UPBUTTONPIN, INPUT);
+  pinMode(DOWNBUTTONPIN, INPUT);
+
+  EEPROM_readAnything(0, configuration);
+  Serial.print("Speed: ");
+  Serial.println(configuration.motorSpeed);
 }
 void loop()
-{
-  // Set motor direction clockwise
-  digitalWrite(dirPin, HIGH);
+{ 
+  
+  int upButtonState = digitalRead(UPBUTTONPIN);  
+  if(upButtonState == HIGH)
+  {    
+    goUp(false);    
+  } else {
+    digitalWrite(UPLEDPIN, LOW);
+  }
+  int downButtonState = digitalRead(DOWNBUTTONPIN);  
+  if(downButtonState == HIGH)
+  {
+    goDown(false);
+  } else {
+    digitalWrite(DOWNLEDPIN, LOW);  
+  }  
+}
+void goUp(bool forced){
+  if(configuration.actualSteps >= minSteps || forced) {
+    configuration.actualSteps--;
+    digitalWrite(UPLEDPIN, HIGH);
+    digitalWrite(DIRPIN, HIGH);
+    go();
+  }
+}
 
-  // Spin motor slowly
+void goDown(bool forced) {
+  if(configuration.actualSteps <= maxSteps || forced) {
+    configuration.actualSteps++;
+    digitalWrite(DOWNLEDPIN, HIGH);
+    digitalWrite(DIRPIN, LOW);
+    go();
+  }  
+}
+
+void go() {
   for(int x = 0; x < stepsPerRevolution; x++)
   {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
+    digitalWrite(STEPPIN, directionUp);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(STEPPIN, LOW);
+    delayMicroseconds(motorSpeed);
   }
-  delay(1000); // Wait a second
-  
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
-
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRevolution*5; x++)
-  {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-  }
-  delay(2500); // Wait a second
+  Serial.print("Steps: ");
+  Serial.println(configuration.actualSteps);
+  EEPROM_writeAnything(0, configuration);
 }
