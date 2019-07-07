f8091f35460dac2bb9997aeec3cd0604ed8bdee0
Viktor Simon-Papp <simonviktor.74@gmail.com>
2019-07-07 09:10:27 +0100

    Clean up master

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -1,6 +1,7 @@
 #include "RF24.h" 
 #include <ArduinoJson.h>
 #include <TimeLib.h>
+#include "C:\Users\viktor\Documents\Projects\SmartHome\arduino\library\ahomePackage.h"
 
 #define RESET_PIN 3
 #define LED 2
@@ -9,27 +10,9 @@ RF24 myRadio (7, 8); // in Mega can use> (48, 49);
 byte addresses[][6] = {"0"}; 
 bool sTime = false;
 
-enum tool {
-  MASTER = 0,
-  BALCONY = 1,
-};
-
-struct package
-{
-  tool id = MASTER;
-  unsigned int packageNum = 0;
-  byte light = 0;  
-  byte h = 0;
-  byte m = 0;
-  byte d = 0;
-  byte mo = 0;
-  byte y = 0;
-  char temperature = 0;
-  char  text[10];  
-};
 typedef struct package Package;
-Package data;
-Package incoming_data;
+Package outgoingData;
+Package incomingData;
 bool ledPin = true;
 
 void setup() {
@@ -40,74 +23,59 @@ void setup() {
   pinMode(LED, OUTPUT);
   delay(100);
   
-  myRadio.begin(); 
-//  myRadio.enableDynamicPayloads();
+  myRadio.begin();
   myRadio.setPayloadSize(128);
   myRadio.setChannel(115); 
   myRadio.setPALevel(RF24_PA_MAX);
   myRadio.setDataRate( RF24_250KBPS ) ; 
   myRadio.openReadingPipe(1, addresses[0]);
   myRadio.openWritingPipe(addresses[0]);
-  myRadio.startListening();  
+  myRadio.startListening();
 }
 
 void loop() {
-  //digitalWrite(LED, ledPin);
   while (Serial.available() > 0) { // if any data available
     String incomingString = Serial.readString();    
-    
+    Serial.println(incomingString);
     const char *buf = incomingString.c_str();
     char *command = strtok((char*)buf, ":");    
     char *value= strtok(NULL, "");
-    
-    if(!strcmp(command, "led")) {
-      if(!strncmp(value, "on", 2)) {
-        digitalWrite(LED, HIGH);
-      }
-  
-      if(!strncmp(value, "off", 3)) {
-        digitalWrite(LED, LOW);
-      }
+
+    if(String(command)== "TEMP") {  
+      myRadio.stopListening();
+      char *receiver = strtok((char*)value, ",");
+      char *ovalue= strtok(NULL, "");
+
+      outgoingData.cmd = TEMP;
+      strcpy(outgoingData.value, ovalue);             
+      strcpy(outgoingData.receiver, receiver);
+      myRadio.write(&outgoingData, sizeof(outgoingData));      
+      myRadio.startListening();
+    }  
+
+    if(String(command) == "DT") {
+      myRadio.stopListening();
+      char *receiver = strtok((char*)value, ",");
+      char *ovalue= strtok(NULL, "");
+
+      outgoingData.cmd = DT;
+      strcpy(outgoingData.value, ovalue);
+      strcpy(outgoingData.receiver, receiver);
+      myRadio.write(&outgoingData, sizeof(outgoingData));      
+      myRadio.startListening();
     }
-
     
-    if(!strcmp(command, "time")) {
-      String val = String(value);
-//      Serial.println(val);
-      
-      String y;
-      String mo;
-      String d;
-      String h;
-      String mi;
-      y = String(value[0]) + String(value[1]) + String(value[2]) + String(value[3]);
-            
-      mo = val[5];
-      if(String(val[4]) != "0") {
-        mo = String(val[4]) + mo;
-        }
+    if(String(command) == "TM") {
+      myRadio.stopListening();
+      char *receiver = strtok((char*)value, ",");
+      char *ovalue= strtok(NULL, "");
 
-      d = String(val[7]);
-      if(String(val[6]) != "0") {
-        d = String(val[6]) + d;
-      }
-
-      h = String(val[9]);
-      if(String(val[8]) != "0") {
-        h = String(val[8]) + h;
-      }
+      outgoingData.cmd = TM;
+      strcpy(outgoingData.value, ovalue);
+      strcpy(outgoingData.receiver, receiver);
+      myRadio.write(&outgoingData, sizeof(outgoingData));      
+      myRadio.startListening();
       
-      mi = String(val[11]);
-      if(String(val[10]) != "0") {
-        mi = String(val[10]) + mi;
-      }
-      
-      
-      setTime(h.toInt(), mi.toInt(), 00, d.toInt(), mo.toInt(), y.toInt());
-      sTime = true;
-    }    
-  }
-
   DynamicJsonDocument myStruct(200);
   char t[10]="test";
   if ( myRadio.available()) 
@@ -115,39 +83,15 @@ void loop() {
     while (myRadio.available())
     {
       digitalWrite(LED, HIGH);
-      myRadio.read( &data, sizeof(data) );
+      myRadio.read( &incomingData, sizeof(incomingData) );
       delay(20);      
       digitalWrite(LED, LOW);      
-    }    
-    
-//    if(sizeof(data.id) == 0){
-//      digitalWrite(RESET_PIN, LOW);
-//    }
-
-    myRadio.stopListening();    
-        
-    //data.light = 44;
-    //data.packageNum = 111;
-    //strcpy(data.text, "Another!");       
-
-
-    if(sTime) {
-      data.h = hour();    
-      data.m = minute();
-      data.d = day();
-      data.mo = month();
-      data.y = year();
-      sTime = false;
     }
-        
-    myRadio.write(&data, sizeof(data));
-    setTime(0, 0, 0, 0, 0, 0);
-    String deviceId(incoming_data.id);
-    String test(t);
-    myStruct["light"] = data.light;
-    myStruct["packageNum"] = data.packageNum;
-    myStruct["id"] = deviceId;
-    myStruct["temperature"] = data.temperature;
+    myRadio.stopListening();
+
+    myStruct["cmd"] = String(incomingData.cmd);
+    myStruct["sender"] = incomingData.sender;
+    myStruct["value"] = incomingData.value;
        
     serializeJson(myStruct, Serial);
     Serial.println();
